bc2bf73c7b8e1087d0558522bbdeafd09bd9e140
Alberto Fung <fungalberto@gmail.com>
2019-02-21 15:34:51 -0600

    added jupyter implementation

--- hough.py
+++ hough.py
@@ -1,24 +1,28 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
 
-imgfile = 'images/detect_circles_soda.jpg'
+imgfile = 'images/multiple.jpg'
 
-image = cv2.imread(imgfile)
+image = cv2.imread(imgfile, 0)
 output = image.copy()
-print(image)
-imagegray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# print(image)
+# imagegray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-circles = cv2.HoughCircles(imagegray, cv2.HOUGH_GRADIENT, 50, 1)
+circles = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT,1,5)
 
-# print(circles)
+print(circles)
 
 if circles is not None:
     circles = np.round(circles[0, :]).astype("int")
 
     for (x, y, r) in circles:
-        cv2.circle(output, (x,y), r, (0, 255, 0), 4)
+        cv2.circle(output, (x,y), r, (150, 0,0), 2)
         # cv2.rectangle(output, ?)
+
+# plt.imshow(image)
 
 cv2.imshow("output", np.hstack([image, output]))
 cv2.waitKey(0)
+cv2.destroyAllWindows()
